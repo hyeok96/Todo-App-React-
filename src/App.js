5cda858cb5d5825e200de60e6134278e95f001f9
이상혁 <leesanghyeok@isanghyeog-ui-MacBookPro.local>
2022-01-21 21:08:19 +0900

    Add remove and fix fuction

--- src/App.js
+++ src/App.js
@@ -25,6 +25,13 @@ const App = () => {
 
   const nextId = useRef(4);
 
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
+
   const onInsert = useCallback(
     (text) => {
       const todo = {
@@ -38,10 +45,21 @@ const App = () => {
     [todos],
   );
 
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 };
